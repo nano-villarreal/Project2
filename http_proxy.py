24d9c3d5d4335fa6f77601e48ccd76c820a1322f
mikiehan <13935460+mikiehan@users.noreply.github.com>
2022-09-24 21:45:21 -0700

    Update http_proxy.py

--- http_proxy.py
+++ http_proxy.py
@@ -100,6 +100,7 @@ def main():
 
     # create the socket for this proxy
     proxy_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    proxy.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     print ("HTTP proxy listening on port ",sys.argv[2])
     proxy_socket.bind(('', int(sys.argv[2])))
     proxy_socket.listen(50) #allow connections  
